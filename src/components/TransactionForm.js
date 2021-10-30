c125c39ee20b86513b05662502a3175f3cfdfc26
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-30 21:04:02 +0100

    form handling

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,8 +1,20 @@
+import { nanoid } from 'nanoid'
 import { useState } from 'react'
 
-const TransactionForm = () => {
+const TransactionForm = ({onNewTransaction}) => {
     const [nameValue, setNameValue] = useState('')
     const [amountValue, setAmountValue] = useState('')
+
+    const addTransaction = (type, evt) => {
+        evt.preventDefault();
+
+        const data = {id: nanoid(), name: nameValue, amount: parseInt(amountValue), type: type};
+
+        onNewTransaction(data)
+
+        setNameValue("");
+        setAmountValue("");
+    }
 
     return (
         <div>
@@ -21,8 +33,8 @@ const TransactionForm = () => {
                     </div>
                 </label>
                 <div>
-                    <button className="income-btn">Add Income</button>
-                    <button className="income-btn">Add Expense</button>
+                    <button className="income-btn" onClick={(e) => addTransaction('income', e)}>Add Income</button>
+                    <button className="income-btn" onClick={(e) => addTransaction('expense', e)}>Add Expense</button>
                 </div>
             </form>
         </div>
