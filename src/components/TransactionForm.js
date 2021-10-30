824032fcec07dbd8e68fa64d982c530504b445c1
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-30 19:57:28 +0100

    changing the inputs to a controlled input

--- src/components/TransactionForm.js
+++ src/components/TransactionForm.js
@@ -1,6 +1,9 @@
-
+import { useState } from 'react'
 
 const TransactionForm = () => {
+    const [nameValue, setNameValue] = useState('')
+    const [amountValue, setAmountValue] = useState('')
+
     return (
         <div>
             <h2>Add New Transaction</h2>
@@ -8,13 +11,13 @@ const TransactionForm = () => {
                 <label htmlFor="Name">
                     Name
                     <div>
-                        <input type="text" />
+                        <input type="text" value={nameValue} onChange={(e) => setNameValue(e.target.value)}/>
                     </div>
                 </label>
                 <label htmlFor="Amount">
                     Amount
                     <div>
-                        <input type="number" />
+                        <input type="number" value={amountValue} onChange={(e) => setAmountValue(e.target.value)}/>
                     </div>
                 </label>
                 <div>
