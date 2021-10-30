eb69c3ae0603f6258c57bb8979c819342ffc4d86
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-30 20:08:53 +0100

    adding state to the aggregator component and the functionslity of calculating expenses

--- src/components/ExpenseTracker.js
+++ src/components/ExpenseTracker.js
@@ -3,20 +3,49 @@ import Expense from "./Expense"
 import TransactionForm from "./TransactionForm"
 import TransactionHistory from "./TransactionHistory"
 
+import { useState, useEffect } from 'react'
+
+// Aggregator component/container component
+
+
 const transactionData = [
     { id: nanoid(),  name: 'salary', amount:3000 , type: 'income' },
     { id: nanoid(),  name: 'Grocery', amount:100 , type: 'Expense' },
 ];
 
 const ExpenseTracker = () => {
+
+    const [income, setIncome] = useState(0)
+    const [expense, setExpense] = useState(0)
+    const [transactions, setTransactions] = useState(transactionData);
+
+    const calculateExpenses = () => {
+        let income = 0, expense = 0;
+
+        transactions.forEach((data) => {
+            if(data.type === 'income') {
+                income += data.amount
+            }else if (data.type === 'expense') {
+                expense += data.amount
+            }
+        })
+        // whenever we change the state of a value in react, the rerender of the component happens
+        // setIncome(income);
+        // setExpense(expense);
+    }
+
+    useEffect(() => {
+        calculateExpenses();
+    }, []);
+
     return (
         <div>
             <h1>Expense Tracker</h1>
-            <Expense income={3000} expense={500}/>
-            <TransactionHistory transactions={transactionData}/>
+            <Expense income={income} expense={expense} />
+            <TransactionHistory transactions={transactions} />
             <TransactionForm />
         </div>
-    )
+    );
 }
 
 export default ExpenseTracker
